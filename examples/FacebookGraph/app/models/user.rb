42ec5c31e19484721f589a89720c27508aff64c2
Clay Allsopp <clay.allsopp@gmail.com>
2012-12-22 10:45:58 -0500

    fix example crash

--- examples/FacebookGraph/app/models/user.rb
+++ examples/FacebookGraph/app/models/user.rb
@@ -1,6 +1,16 @@
 class User < Maglev::Model
   remote_attributes :id, :name, :bio
 
+  has_many :friends,
+    class_name: "User",
+    collection_path: "/me/friends",
+    member_path: "/:id",
+    json_path: "data"
+
+  has_one :wall,
+    class_name: "Post",
+    member_path: "/posts"
+
   collection_path ""
   member_path "/:id"
 end
