7297dab7d1392d0d92e519d39ffaca8649d2fbd0
Clay Allsopp <clay.allsopp@gmail.com>
2012-12-22 10:45:48 -0500

    fix the chained dsl

--- lib/maglev/model/relationships.rb
+++ lib/maglev/model/relationships.rb
@@ -1,5 +1,25 @@
 module Maglev
   module Relationships
+    module Support
+      module_function
+
+      def attach_model_dsl(model_value, klass, _collection_path, _member_path)
+        # Attach the DSL to the model
+        # i.e. user.friends.remote_find_all do
+        # end
+        model_value.extend(Maglev::ModelUrls)
+        model_value.collection_path(_collection_path)
+        model_value.member_path(_member_path)
+        model_value.extend(Maglev::API::HTTP)
+        model_value.define_singleton_method "http_call", -> (method, url, call_options = {}, &block) {
+          klass.http_call(method, url, call_options, &block)
+        }
+        model_value.extend(Maglev::Record)
+        model_value.define_singleton_method "create_model" {
+          klass.create_model(json)
+        }
+      end
+    end
     # OPTIONS
     # - class_name
     # - json_path
@@ -9,27 +29,7 @@ module Maglev
       @remote_has_one ||= {}
 
       if name && name = name.to_s
-        klass, _collection_path, _member_path = build_relationship("@remote_has_one", name, options)
-
-        # GETTER
-        ivar = "@#{name}"
-        attr_reader name
-
-        # SETTER
-        define_method("#{name}=") do |value|
-          model_value = Maglev::Support::TransformHash.transform(value, klass)
-  
-          # EX
-          # user.feed = a_feed
-          # then we set feed.user = user
-          if model_value.class.remote_belongs_to? self.class and !blocked?
-            model_value.block_in do
-              model_value.send("#{self.class.snake_case}=", self)
-            end
-          end
-
-          instance_variable_set(ivar, model_value)
-        end
+        create_singular_methods(name, options, "remote_has_one")
       end
 
       @remote_has_one
@@ -45,26 +45,7 @@ module Maglev
       @remote_belongs_to ||= {}
 
       if name && name = name.to_s
-        klass, _collection_path, _member_path = build_relationship("@remote_belongs_to", name, options)
-
-        ivar = "@#{name}"
-        attr_reader name
-
-        # EX
-        # feed.user = a_user
-        # then we set
-        # user.feed = feed
-        define_method("#{name}=") do |value|
-          model_value = Maglev::Support::TransformHash.transform(value, klass)
-
-          if model_value.class.remote_has_one? self.class and !blocked?
-            model_value.block_in do
-              model_value.send("#{self.class.snake_case}=", self)
-            end
-          end
-
-          instance_variable_set(ivar, model_value)
-        end
+        create_singular_methods(name, options, "remote_belongs_to")
       end
 
       @remote_belongs_to
@@ -103,7 +84,11 @@ module Maglev
 
             instance_variable_set(ivar, arr)
           end
-          instance_variable_get(ivar)
+          model_value = instance_variable_get(ivar)
+
+          Maglev::Relationships::Support.attach_model_dsl(model_value, klass, _collection_path, _member_path)
+
+          model_value
         end
 
         define_method("#{name}=") do |values|
@@ -119,14 +104,49 @@ module Maglev
     end
 
     private
+    def create_singular_methods(name, options, method)
+      klass, _collection_path, _member_path = build_relationship("@#{method}", name, options)
+
+      ivar = "@#{name}"
+      define_method("#{name}") do
+        model_value = instance_variable_get(ivar)
+
+        Maglev::Relationships::Support.attach_model_dsl(model_value, klass, _collection_path, _member_path)
+
+        model_value
+      end
+
+      # EX
+      # feed.user = a_user
+      # then we set
+      # user.feed = feed
+      opposite_method = {"remote_belongs_to" => "remote_has_one", "remote_has_one" => "remote_belongs_to"}[method]
+      define_method("#{name}=") do |value|
+        model_value = Maglev::Support::TransformHash.transform(value, klass)
+
+        if model_value.class.send("#{opposite_method}?", self.class) and !blocked?
+          model_value.block_in do
+            model_value.send("#{self.class.snake_case}=", self)
+          end
+        end
+
+        instance_variable_set(ivar, model_value)
+      end
+    end
+
     def build_relationship(ivar, name, options, singular = false)
+      # EX: 'friends' -> {}['friends']
       json_path = options[:json_path] || name
 
+      # EX: 'friends' -> 'Friends', 'wall_post' -> 'WallPost'
       class_name = options[:class_name] || Maglev::Support.classify_string(singular ? name.singularize : name)
       klass = Maglev::Support.get_or_make_class(class_name)
+      # If the klass already exists, the klass.collection_path will also exist. else nil.
       _collection_path = options[:collection_path] || klass.collection_path
       _member_path = options[:member_path] || klass.member_path
 
+      # EX: '@remote_has_many['friends'] = {}...'
+      # Applied to each model
       instance_variable_get(ivar)[name] = {
         class: klass,
         json_path: json_path,
@@ -135,40 +155,6 @@ module Maglev
       }
 
       [klass, _collection_path, _member_path]
-    end
-
-    def add_dsl_extension_attr(name, klass, _collection_path, _member_path)
-      alias_method "old_#{name}", name
-
-      define_method("#{name}") do
-        value = self.send("old_#{name}")
-        self.class.send(:add_dsl_extension, value, klass, _collection_path, _member_path)
-        value
-      end
-    end
-
-    def add_dsl_extension(object, klass, _collection_path, _member_path)
-      # Add some DSL finders to it
-      # EX
-      # user.feeds.find_all do |feeds|
-      #   ...
-      # end
-      # Give it the HTTP methods
-      object.send(:extend, Maglev::API::HTTP)
-      # Defer http_call to the resource class
-      def object.http_call(method, url, call_options = {}, &block)
-        klass.http_call(method, url, call_options, &block)
-      end
-      # Give it the AR finders
-      object.send(:extend, Maglev::Record)
-      # Give it the _path helpers
-      object.send(:extend, Maglev::ModelUrls)
-      object.collection_path _collection_path
-      object.member_path _member_path
-      # use this resource Klass, not the object's class
-      def object.create_model(json)
-        klass.new(json)
-      end
     end
   end
 
